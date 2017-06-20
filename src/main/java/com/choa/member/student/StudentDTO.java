74e0d30b2a74b6dacca739ac72a12fcd53582bcd
user2-PC\user2 <dbrud1336@gmail.com>
2017-06-20 21:32:07 +0900

    DTO생성

---
+++ src/main/java/com/choa/member/student/StudentDTO.java
@@ -0,0 +1,34 @@
+package com.choa.member.student;
+
+public class StudentDTO {
+	
+	private String sid;
+	private String phone;
+	private String tid;
+	
+	
+	public String getSid() {
+		return sid;
+	}
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getTid() {
+		return tid;
+	}
+	public void setTid(String tid) {
+		this.tid = tid;
+	}
+	
+	
+	
+	
+	
+
+}
